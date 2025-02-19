5993691e6751e1818b2a8dd3b37acffee2be8eff
Aye10032 <tc_aye@outlook.com>
2025-02-20 02:59:05 +0800

    移除checkpoint字段

--- app/schemas/write_project.py
+++ app/schemas/write_project.py
@@ -6,7 +6,6 @@ from sqlmodel import SQLModel, Field
 
 class WriteProject(SQLModel):
     uid: str = Field(unique=True)
-    graph_checkpoint: str
     user_email: str
     description: str
     last_manuscript: str = ''
