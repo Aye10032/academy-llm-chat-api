03b86bb13a2f616b1cc7515e844887fb8405a74a
Aye10032 <tc_aye@outlook.com>
2025-03-16 22:23:29 +0800

    删除无用数据表结构

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,13 +4,10 @@ __all__ = [
     'ChatSessionTable',
     'WriteProjectTable',
     'ManuscriptTable',
-    'PubMedPaperTable',
-    'PubMedReferenceTable',
 ]
 
 from app.models.chat_session import ChatSessionTable
 from app.models.knowledge_base import KnowledgeBaseTable
 from app.models.manuscript import ManuscriptTable
-from app.models.pubmed import PubMedPaperTable, PubMedReferenceTable
 from app.models.user import UserTable
 from app.models.write_project import WriteProjectTable
